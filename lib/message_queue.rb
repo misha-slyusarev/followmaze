ec4d6be490808c76b44642eb1ba687776848d548
Misha Slyusarev <mikhail.slyusarev@c3energy.com>
2016-06-28 00:30:13 +0300

    Add drop method and queue attribute

--- lib/message_queue.rb
+++ lib/message_queue.rb
@@ -1,11 +1,19 @@
 class MessageQueue
+  attr_writer :queue
+
   def initialize(client_socket)
     @socket = client_socket.accept
+    @queue = Queue.new
+
     puts 'Client connected'
   end
 
   def run
-    @socket.write('HELLO')
+    @socket.puts('HELLO')
+  end
+
+  def drop(message)
+    @socket.puts(message)
     @socket.close
   end
 end
