75b2e84f06b2096d2189a9ac8e6784fd6b8eaebc
Misha Slyusarev <mikhail.slyusarev@c3energy.com>
2016-06-29 13:32:45 +0300

    Implement find_message_queue

--- lib/router.rb
+++ lib/router.rb
@@ -10,30 +10,41 @@ class Router
   end
 
   def convey(message)
-    #puts " -> #{message.type} from #{message.from} to #{message.to}"
+    puts " -> #{message.type} from #{message.from} to #{message.to}"
 
-    case message.type
-    when 'F'
-      mq = @message_queues.bsearch { |mq| mq.id == message.to }
-      mq.add_follower(message.from)
-      mq.push(message.raw)
-    when 'U'
-      mq = @message_queues.bsearch { |mq| mq.id == message.to }
-      mq.remove_follower(message.from)
-    when 'B'
-      @message_queues.each { |mq| mq.push(body) }
-    when 'P'
-      send_message(message.to, message.raw)
-    when 'S'
-      mq = @message_queues.bsearch { |mq| mq.id == message.from }
-      mq.each_follower { |fid| send_message(fid, message.raw)}
+    begin
+      case message.type
+      when 'F'
+        mq = find_message_queue(message.to)
+        mq.add_follower(message.from)
+        mq.push(message.raw)
+      when 'U'
+        mq = find_message_queue(message.to)
+        mq.remove_follower(message.from)
+      when 'B'
+        @message_queues.each { |mq| mq.push(body) }
+      when 'P'
+        send_message(message.to, message.raw)
+      when 'S'
+        mq = find_message_queue(message.from)
+        mq.followers.each { |fid| send_message(fid, message.raw)}
+      end
+    rescue NoQueueFound
     end
   end
 
 private
 
   def send_message(id, body)
-    mq = @message_queues.bsearch { |mq| mq.id == id }
-    mq.push(body)
+    message_queue = @message_queues.bsearch { |mq| id - mq.id } or raise NoQueueFound
+    message_queue.push(body)
   end
+
+  def find_message_queue(id)
+    mq = @message_queues.bsearch { |mq| id - mq.id }
+    mq or raise NoQueueFound
+  end
+end
+
+class NoQueueFound < StandardError
 end
