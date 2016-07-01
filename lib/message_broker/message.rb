256283502d3a9ed559943be7a933666e843fe10b
Misha Slyusarev <mikhail.slyusarev@c3energy.com>
2016-07-01 21:54:43 +0300

    Add description for Message class

--- lib/message_broker/message.rb
+++ lib/message_broker/message.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 # frozen_string_literal: true
 module MessageBroker
+  # Message parses raw line recieved from event sourse
+  # and caries this information for futher processing
   class Message
     attr_reader :sequence, :type, :from, :to, :raw
 
