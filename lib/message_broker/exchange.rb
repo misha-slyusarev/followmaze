ebd58652d3c017748ec5e4975edcca36482816b7
Misha Slyusarev <mikhail.slyusarev@c3energy.com>
2016-07-01 21:44:01 +0300

    Granulate 'Exchange.convey' method

--- lib/message_broker/exchange.rb
+++ lib/message_broker/exchange.rb
@@ -1,6 +1,10 @@
 # encoding: utf-8
 # frozen_string_literal: true
+
 module MessageBroker
+  # Exchange routes messages across clients. It also managing a
+  # situation when there is no client to follow to. It then
+  # creates virtual queues that are able to manage real followers
   class Exchange
     attr_reader :message_queues
 
@@ -9,50 +13,62 @@ module MessageBroker
     end
 
     def convey(message)
-      case message.type
+      @current_message = message
 
-      when Message::Type::FOLLOW
-        to_mq = find_message_queue(message.to)
-        from_mq = find_message_queue(message.from)
-        to_mq.followers << from_mq.id
-        to_mq.push(message.raw)
-
-      when Message::Type::UNFOLLOW
-        mq = find_message_queue(message.to)
-        mq.followers.delete(message.from)
-        if !mq.followers && mq.class == VirtualMessageQueue
-          @message_queues.delete(mq)
-        end
-
-      when Message::Type::BROADCAST
-        @message_queues.each { |mq| mq.push(message.raw) }
-
-      when Message::Type::PRIVATE
-        send_message(message.to, message.raw)
-
-      when Message::Type::STATUS
-        mq = find_message_queue(message.from)
-        mq.followers.each { |id| send_message(id, message.raw) }
+      case @current_message.type
+      when Message::Type::FOLLOW then subscribe
+      when Message::Type::UNFOLLOW then unsubscribe
+      when Message::Type::BROADCAST then broadcast
+      when Message::Type::PRIVATE then send_private
+      when Message::Type::STATUS then send_status
       end
 
     rescue NoQueueFound
       return unless Message::Type::FOLLOW == message.type
-
-      # If there is a reciever with 'from' id, then we want
-      # to create a virtual queue for him to follow.
-      #
-      # If there is no, and 'to' exists, then just send raw
-      # Follow message out to 'to' reciever
-      if @message_queues.bsearch { |mq| message.from - mq.id }
+      # Sometimes there is no client to follow to. In this case
+      # if a subscriber with 'from' id exists, we want to create
+      # a virtual queue for him to subscribe.
+      # If there is no such a client, and 'to' exists, then just
+      # send raw 'Follow' message out to the client with 'to' id
+      if @message_queues.bsearch { |mq| @current_message.from - mq.id }
         vmq = VirtualMessageQueue.new(message.to)
-        vmq.followers << message.from
+        vmq.followers << @current_message.from
         @message_queues << vmq
-      elsif to_mq
-        to_mq.push(message.raw)
+      else
+        to_mq = @message_queues.bsearch { |mq| @current_message.to - mq.id }
+        to_mq.push(message.raw) unless to_mq.nil?
       end
     end
 
     private
+
+    def subscribe
+      to_mq = find_message_queue(@current_message.to)
+      from_mq = find_message_queue(@current_message.from)
+      to_mq.followers << from_mq.id
+      to_mq.push(@current_message.raw)
+    end
+
+    def unsubscribe
+      mq = find_message_queue(@current_message.to)
+      mq.followers.delete(@current_message.from)
+      if !mq.followers && mq.class == VirtualMessageQueue
+        @message_queues.delete(mq)
+      end
+    end
+
+    def broadcast
+      @message_queues.each { |mq| mq.push(@current_message.raw) }
+    end
+
+    def send_private
+      send_message(@current_message.to, @current_message.raw)
+    end
+
+    def send_status
+      mq = find_message_queue(@current_message.from)
+      mq.followers.each { |id| send_message(id, @current_message.raw) }
+    end
 
     def send_message(id, body)
       mq = find_message_queue(id)
