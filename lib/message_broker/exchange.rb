e224a445740723b150c171e72c22e58f93d58fdc
Misha Slyusarev <mikhail.slyusarev@c3energy.com>
2016-07-01 13:39:07 +0300

    Don't add defunct queues to followers list

--- lib/message_broker/exchange.rb
+++ lib/message_broker/exchange.rb
@@ -7,40 +7,54 @@ module MessageBroker
     end
 
     def convey(message)
-      begin
-        case message.type
-        when Message::Type::FOLLOW
-          mq = find_message_queue(message.to)
-          mq.followers << message.from
-          mq.push(message.raw)
-        when Message::Type::UNFOLLOW
-          mq = find_message_queue(message.to)
-          mq.followers.delete(message.from)
-          if ! mq.followers && mq.class == VirtualMessageQueue
-            @message_queues.delete(mq)
-          end
-        when Message::Type::BROADCAST
-          @message_queues.each { |mq| mq.push(message.raw) }
-        when Message::Type::PRIVATE
-          send_message(message.to, message.raw)
-        when Message::Type::STATUS
-          mq = find_message_queue(message.from)
-          mq.followers.each { |fid| send_message(fid, message.raw)}
+      case message.type
+
+      when Message::Type::FOLLOW
+        to_mq = find_message_queue(message.to)
+        from_mq = find_message_queue(message.from)
+        to_mq.followers << from_mq.id
+        to_mq.push(message.raw)
+
+      when Message::Type::UNFOLLOW
+        mq = find_message_queue(message.to)
+        mq.followers.delete(message.from)
+        if ! mq.followers && mq.class == VirtualMessageQueue
+          @message_queues.delete(mq)
         end
-      rescue NoQueueFound
-        if Message::Type::FOLLOW == message.type
-          vmq = VirtualMessageQueue.new(message.to)
-          vmq.followers << message.from
-          @message_queues << vmq
-        end
+
+      when Message::Type::BROADCAST
+        @message_queues.each { |mq| mq.push(message.raw) }
+
+      when Message::Type::PRIVATE
+        send_message(message.to, message.raw)
+
+      when Message::Type::STATUS
+        mq = find_message_queue(message.from)
+        mq.followers.each { |id| send_message(id, message.raw)}
+      end
+
+    rescue NoQueueFound
+      return unless Message::Type::FOLLOW == message.type
+
+      # If there is a reciever with 'from' id, then we want
+      # to create a virtual queue for him to follow.
+      #
+      # If there is no, and 'to' exists, then just send raw
+      # Follow message out to 'to' reciever
+      if @message_queues.bsearch { |mq| message.from - mq.id }
+        vmq = VirtualMessageQueue.new(message.to)
+        vmq.followers << message.from
+        @message_queues << vmq
+      elsif to_mq
+        to_mq.push(message.raw)
       end
     end
 
   private
 
     def send_message(id, body)
-      mq = @message_queues.bsearch { |mq| id - mq.id }
-      mq.push(body) unless mq.nil?
+      mq = find_message_queue(id)
+      mq.push(body)
     end
 
     def find_message_queue(id)
